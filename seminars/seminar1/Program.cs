a452a865be60f47ba2827a82d9fd09f39ea580e2
Alik <yanshin.alek@yandex.ru>
2023-03-01 00:23:23 +0300

    Семинар1. Начало

--- seminars/seminar1/Program.cs
+++ seminars/seminar1/Program.cs
@@ -2,3 +2,5 @@
 int num = 5;
 int num2 = Convert.ToInt32(Console.ReadLine()); //преобразование из строчного в целый тип числа(взятые данные из консоли всегда строчные)
 Console.WriteLine(num + " " + num2);
+
+
