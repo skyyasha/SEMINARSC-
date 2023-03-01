439ae677fc8e619ec0a72b47560fb9860d4b2d3a
Alik <yanshin.alek@yandex.ru>
2023-03-02 00:35:28 +0300

    Пробное

--- seminars/seminar1/Program.cs
+++ seminars/seminar1/Program.cs
@@ -1,7 +1,32 @@
-﻿Console.WriteLine("Input number");
+﻿// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int quad = num2 * num2;
+// if(quad == num1)
+// {
+//    Console.WriteLine("First numbers is quad of second number");
+// } 
+// else
+// {
+//    Console.WriteLine("First numbers is not quad of second number");
+// }
+            //Задача3
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = num * (-1);
+
+// while (current <= num)
+// {
+//    Console.Write(current + " ");
+//    current++; // == current + 1
+// }
+
+                     
+Console.WriteLine("Input number three-didgit number");
 int num = Convert.ToInt32(Console.ReadLine());
-int quad = num * num;
-Console.WriteLine($"Quad of {num} is {quad}");
 
+int resalt = num % 10;
 
-
+Console.WriteLine($"Last number of {num} is {resalt}");
