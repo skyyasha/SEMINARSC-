70563682bdc51b4221c953893fa6552fcbf4a394
Alik <yanshin.alek@yandex.ru>
2023-03-01 22:49:49 +0300

    сохранение задачи

--- seminars/seminar1/Program.cs
+++ seminars/seminar1/Program.cs
@@ -1,6 +1,7 @@
-﻿Console.WriteLine("GB lesson");
-int num = 5;
-int num2 = Convert.ToInt32(Console.ReadLine()); //преобразование из строчного в целый тип числа(взятые данные из консоли всегда строчные)
-Console.WriteLine(num + " " + num2);
+﻿Console.WriteLine("Input number");
+int num = Convert.ToInt32(Console.ReadLine());
+int quad = num * num;
+Console.WriteLine($"Quad of {num} is {quad}");
+
 
 
